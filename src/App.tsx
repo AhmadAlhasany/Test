f723270efffcced988b20e7e5bac2fc0a75f8180
Ahmad Alhasany <ahmadhs271@gmail.com>
2024-11-23 01:52:10 +0300

    refactor: moved products fetch logic to the App component
    - this will help me use products info when navigating to the detailed page, since there isn't an api to get a specific product details

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,21 @@ import Products from './pages/products/Products';
 import Profile from './pages/profile/Profile';
 import Login from './pages/auth/login/Login';
 import Layout from './components/layout/Layout';
+import ProductDetails from './pages/productDetails/ProductDetails';
+import { useProducts } from './hooks/useProducts';
 
 
 function App() {
+  const {isLoading, products} = useProducts();
+
   return (
     <Router>
       <div className="App">
         <Toaster />
         <Routes>
           <Route path="/" element={<Layout/>}>
-            <Route path="/" element={<Products />} />
+            <Route path="/" element={<Products isLoading={isLoading} products={products} />} />
+            <Route path='/:id' element={<ProductDetails products={products}/>}/>
             <Route path="/profile" element={<Profile />}/>
           </Route>
           <Route path="/login" element={<Login />} />
--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,10 +1,10 @@
 import ProductCard from '../../components/products/ProductCard';
 import { useProducts } from '../../hooks/useProducts'
+import { productsProps } from '../../models/products';
 import './products.css'
 
-export default function Products() {
-  const {isLoading, products} = useProducts();
-
+export default function Products(props:productsProps) {
+  const {isLoading, products} = props
   return (
     <div className='products'>
       {isLoading && <h3 className=' col-span-full text-center text-2xl '>Loading...</h3>}
